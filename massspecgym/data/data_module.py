4f6da4dfe5285a77d8fd33fe035f6086b894780e
roman-bushuiev <ro3721@gmail.com>
2025-11-10 01:06:43 +0200

    Split setup in MassSpecDataModule

--- massspecgym/data/data_module.py
+++ massspecgym/data/data_module.py
@@ -42,38 +42,46 @@ class MassSpecDataModule(pl.LightningDataModule):
         """Pre-processing to be executed only on a single main device when using distributed training."""
         pass
 
-    def setup(self, stage=None):
-        """Pre-processing to be executed on every device when using distributed training."""
+    def _prepare_split_mask(self):
 
         # Prepare split
         if self.split_pth is None:
-            self.split = self.dataset.metadata[["identifier", "fold"]]
+            df_split = self.dataset.metadata[["identifier", "fold"]]
         else:
             # NOTE: custom split is not tested
-            self.split = pd.read_csv(self.split_pth, sep="\t")
-            if set(self.split.columns) != {"identifier", "fold"}:
+            df_split = pd.read_csv(self.split_pth, sep="\t")
+            if set(df_split.columns) != {"identifier", "fold"}:
                 raise ValueError('Split file must contain "id" and "fold" columns.')
-            self.split["identifier"] = self.split["identifier"].astype(str)
-            if set(self.dataset.metadata["identifier"]) != set(self.split["identifier"]):
+            df_split["identifier"] = df_split["identifier"].astype(str)
+            if set(self.dataset.metadata["identifier"]) != set(df_split["identifier"]):
                 raise ValueError(
                     "Dataset item IDs must match the IDs in the split file."
                 )
 
-        self.split = self.split.set_index("identifier")["fold"]
-        if not set(self.split) <= {"train", "val", "test"}:
+        df_split = df_split.set_index("identifier")["fold"]
+        if not set(df_split) <= {"train", "val", "test"}:
             raise ValueError(
                 '"Folds" column must contain only "train", "val", or "test" values.'
             )
 
         # Split dataset
-        split_mask = self.split.loc[self.dataset.metadata["identifier"]].values
+        self.split_mask = df_split.loc[self.dataset.metadata["identifier"]].values
+
+    def _split_dataset(self, stage=None):
         if stage == "fit" or stage is None:
             self.train_dataset = Subset(
-                self.dataset, np.where(split_mask == "train")[0]
+                self.dataset, np.where(self.split_mask == "train")[0]
             )
-            self.val_dataset = Subset(self.dataset, np.where(split_mask == "val")[0])
-        if stage == "test":
-            self.test_dataset = Subset(self.dataset, np.where(split_mask == "test")[0])
+            self.val_dataset = Subset(self.dataset, np.where(self.split_mask == "val")[0])
+        elif stage == "test":
+            self.test_dataset = Subset(self.dataset, np.where(self.split_mask == "test")[0])
+        else:
+            raise ValueError(f"Invalid stage: {stage}")
+
+    def setup(self, stage=None):
+        """Pre-processing to be executed on every device when using distributed training."""
+        self._prepare_split_mask()
+        self._split_dataset(stage)
 
     def train_dataloader(self):
         return DataLoader(
